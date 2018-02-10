fab199e22cfef7ff3c5f7fb649d2c08b58e8a897
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2018-02-10 16:21:53 -0500

    images are displaying in grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import './style.css';
+import Grid from "./components/Grid";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import lorList from "./lor.json";
 
 class App extends Component {
+state = {
+    lorList,
+    currentScore: 0,
+    record: 0,
+    message: "",
+    classShake: "",
+};
+
   render() {
     return (
-      <div>
-          <h2>Welcome to React</h2>
-        <p>
-          Hello World
-        </p>
-      </div>
+      <Wrapper>
+        <Title>Choose Your Character</Title>
+        {this.state.lorList.map(lorChar => (
+          <Grid
+            id={lorChar.id}
+            key={lorChar.id}
+            name={lorChar.name}
+            image={lorChar.image}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
---
+++ client/src/components/Grid.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Grid.css";
+
+const Grid = props => (
+  <div className="card">
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+  </div>
+);
+
+export default Grid;
